1a26bce60c4b116bffd7c67c4c1933103a46c98e
mzf11125 <mzidanfatonie@gmail.com>
2024-01-31 23:58:09 +0700

    Initialize

---
+++ learning-go/first.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//int 16 = 16 bytes
+//int 32 = 32 bytes
+// int 64 = 64 bytes
+
+func main() {
+	// fmt.Println("Hello World")
+	var intNum int16 = 20
+	var intNum2 int32 = 30
+	var intNum3 int64 = 40
+
+
+	fmt.Println(intNum)
+	fmt.Println(intNum2)
+	fmt.Println(intNum3)
+}
+
