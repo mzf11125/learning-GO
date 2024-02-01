1fffe864f24ea6bc7005d935fd95e76d371faef4
mzf11125 <mzidanfatonie@gmail.com>
2024-02-01 16:21:06 +0700

    Test

--- learning-go/first.go
+++ learning-go/first.go
@@ -5,6 +5,30 @@ import "fmt"
 //int 16 = 16 bytes
 //int 32 = 32 bytes
 // int 64 = 64 bytes
+//floatNum float32 = 32 bytes
+//floatNum float64 = 64 bytes
+//string = 32 bytes
+//bool = 1 byte (0 or 1)
+//+ - * / % ++ -- += -= *= /= %= == != > < >= <= && || ! & | ^ << >>
+//Cannot do mix type operations in Go like floatnum32 + intnum16
+
+// String type to store text
+// var stringName string = "Hello World"
+//Double quotes for string
+//Single quotes for characters
+//Back quote
+//Int division rounded down
+
+//String type to store string
+// var stringName string = "Hello World"
+//Double quotes for string
+// concatenate string using +
+//len number of bytes -> Outside of ASCII, it will not work properly. It's based on the encoding used. UTF-8 uses one byte per character. So if you have a string with only English letters and numbers, it will work properly. If you have a string with Chinese characters, it will not work properly.
+//String to byte array
+//RuneCOuntintString function
+//Rune is a character in GoLang
+//Int
+
 
 func main() {
 	// fmt.Println("Hello World")
